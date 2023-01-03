93c40f7a3aa7a6c3c99c0d820a4f4900ee349b47
Yuriy Dupyn <2153100+omedusyo@users.noreply.github.com>
2023-01-03 23:32:31 +0100

    Counter component

---
+++ src/Component/Counter.purs
@@ -0,0 +1,80 @@
+module Component.Counter where
+
+import Prelude
+
+import Halogen as H
+import Halogen.HTML as H
+import Halogen.HTML.Events as HE
+
+-------------Input-----------------
+-- This is not Msg.
+-- Our counter component is self-contained. We don't take inputs from a parent. Wait... Shouldn't then the type be Empty?
+-- TODO
+-- type Input = Void
+-- no... Void doesn't work, because the initial model takes Input as a parameter.
+--
+-- So what exactly is this input? Is it the initial read-only configuration?
+-- Or can I actually resend the input?
+type Input = Unit
+
+-------------Model-----------------
+type Model =
+  { count :: Int
+  }
+
+initModel :: Input -> Model
+initModel _ =
+  { count: 0
+  }
+
+-------------Msg-----------------
+-- Note that this is internal message. Input is something else - input from the parent component!
+-- Since this is internal, maybe a better name would be Event? Or InternalEvent?
+data Msg =
+    Increment
+  | Decrement
+
+-------------Update-----------------
+-- update :: Msg -> Model -> Model
+-- TODO:
+-- * How is the communication with other components done in update?
+-- * Can I spawn new components in the update?
+-- TODO:
+-- * `m`? This is the effect monad
+-- * `output` This is for parent communication.
+-- * () means no child components that we can talk to. Use `slots` as a convential type var for it.
+update :: forall output m . Msg -> H.HalogenM Model Msg () output m Unit
+update =
+  case _ of
+    Increment ->
+      H.modify_ \model -> model { count = model.count + 1 }
+
+    Decrement ->
+      H.modify_ \model -> model { count = model.count - 1 }
+
+-------------VIEWS-----------------
+
+-- view :: forall w i . Model -> H.HTML w i
+view :: forall m . Model -> H.ComponentHTML Msg () m
+view model =
+  H.div []
+    [ H.text $ show model.count
+    , H.button [ HE.onClick \_ -> Increment ] [ H.text "inc" ]
+    , H.button [ HE.onClick \_ -> Decrement ] [ H.text "dec" ]
+    ]
+
+-------------Component-----------------
+
+-- TODO: What is
+-- * `query`? A way to communicate TO the parent
+-- * `output` a way that the parent can communicate with the component
+-- Interesting... there's no Msg, it is internal... This is the public interface of a component.
+--
+-- Note that there is nothing about subcomponents...
+component :: forall query output m . H.Component query Input output m
+component =
+  H.mkComponent
+    { initialState: initModel
+    , render: view
+    , eval: H.mkEval H.defaultEval { handleAction = update }
+    }
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,66 @@ module Main where
 
 import Prelude
 
+import Data.Maybe ( Maybe(..) )
+
 import Effect (Effect)
 import Effect.Console (log)
 
+import Halogen.HTML as H
+import Halogen.HTML.Properties as HP
+
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+
+import Component.Counter as Counter
+
 main :: Effect Unit
-main = do
-  log "Hello, World!"
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  -- log "Hello, World!"
+  runUI Counter.component unit body
+
+-------------VIEWS-----------------
+
+-- `HTML w i` is the type used for html that's not tied to a particular component.
+--  `w` ~ widget, describes which sub-components can be used. What is the kind of `w`?
+--  `i` ~ input, is this like msg?
+
+element :: forall (w :: Type) (i :: Type) . H.HTML w i
+element =
+  H.h1 [] [ H.text "Hello, World!" ]
+
+
+-- element1 :: forall w i . H.HTML w i
+element1 :: H.PlainHTML
+element1 =
+  H.div [ HP.id "root" ]
+    [ H.input  [ HP.placeholder "Name" ]
+    , H.button [ HP.classes [ H.ClassName "btn-primary" ], HP.type_ HP.ButtonSubmit ]
+        [ H.text "Submit" ]
+    ]
+
+-- test0 :: forall w msg . H.HTML w msg
+-- test0 =
+--   H.div [ HP.placeholder "wat" ]
+--     []
+
+header :: forall w i . Int -> H.HTML w i
+header visits =
+  H.h1 []
+    [ H.text $ "You've had " <> show visits <> " visitors" ]
+
+
+maybeElem :: forall w i a . Maybe a -> (a -> H.HTML w i) -> H.HTML w i
+maybeElem a f =
+  case a of
+    Just x -> f x
+
+    Nothing -> H.text ""
+
+whenElem :: forall w i. Boolean -> (Unit -> H.HTML w i) -> H.HTML w i
+whenElem cond f =
+  if cond then
+    f unit
+  else
+    H.text ""
